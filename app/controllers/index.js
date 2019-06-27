cfd45b8aa8bd34b91b1a5698019785b1ee94c9fb
superkyoh <yohanagheringer@gmail.com>
2019-06-27 13:01:41 -0300

    created rpg got

---
+++ app/controllers/index.js
@@ -0,0 +1,23 @@
+module.exports.index = function(application, req, res){
+    res.render('index', {validacao:{}});
+}
+
+module.exports.autenticar = function(application, req, res){
+    var dadosForm = req.body;
+
+    req.assert('usuario', 'Usuário não pode estar vazio').notEmpty();
+    req.assert('senha', 'Senha não pode estar vazia').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('index', {validacao: erros});
+        return;
+    }
+
+    var connection = application.config.dbConnection;
+    var UsuariosDAO = new application.app.models.UsuariosDAO(connection);
+
+    UsuariosDAO.autenticar(dadosForm, req, res);
+
+}
